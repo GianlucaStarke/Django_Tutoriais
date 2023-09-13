b2bed19984f9b11a5fc3198e6fb8b9b158b5e640
Gianluca Starke <gianlucastarke@gmail.com>
2023-09-13 19:42:13 -0300

    Adicionar tela de deletar post

--- Tutorial_06/blog/views.py
+++ Tutorial_06/blog/views.py
@@ -1,9 +1,11 @@
 from .models import Post
+from django.urls import reverse_lazy
 from django.views.generic import (
     ListView,
     DetailView,
     CreateView,
     UpdateView,
+    DeleteView,
 )
 
 class BlogListView(ListView):
@@ -23,3 +25,8 @@ class BlogUpdateView(UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
